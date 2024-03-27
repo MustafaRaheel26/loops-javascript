ed80fac5e01197a7a594327ce1b06bb2479d8aa5
MustafaRaheel26 <mustafaraheel26@gmail.com>
2024-03-26 17:28:57 -0700

    mustfafa

---
+++ app.js
@@ -0,0 +1,137 @@
+    // Question  1
+
+// for (let i=1 ; i<=5; i++){
+//     console.log(`Hello World ${i}`);
+// }
+
+    //   Question  2
+
+    // for (let i=1 ; i<=10; i++){
+    //     console.log(i);
+    // }
+
+
+    // Qquestion  3
+
+    // const tableNum =+prompt("Enter a table num");
+    // const length =+prompt("Enter table length ");
+
+    // for ( let i=1 ; i<=length ; i++){
+    //     console.log(`${tableNum} * ${i} = ${tableNum*i}`);
+    // }
+
+
+    // Question  4 
+
+//    let A =["Nokia","Samsung","Apple","Sony","Huawei"];
+
+//    for (let i=0;i<A.length;i++){
+//      console.log(`${A[i]}`);
+//    }
+
+
+        // Question 5
+
+
+//    let A =["Apple","Banana","Mango","Orange","Strawberry"];
+
+//    for (let i=0;i<A.length;i++){
+//      console.log(`${A[i]}`);
+//    }
+//      for (let j=0;j<A.length;j++){
+//         console.log(`Element at index ${j} is ${A[j]}`);
+//      }
+   
+
+    //   Question  6
+
+
+    // const userInput=+prompt("Enter Number of Items");
+    // const arr=[];
+
+    // for (let i=0;i<userInput;i++) {
+    //     const arrayItems =prompt("Enter item you want to push into an array");
+    //     arr.push(arrayItems);
+    //  }    
+    //  console.log(arr);
+
+
+    // Question   7
+
+    // for (let i=1;i<=15;i++){
+    //     console.log(`Counting ${i}`);
+    // }
+
+    // for (let i=10;i>=1;i--){
+    //     console.log(`Reverse Counting ${i}`);
+    // }
+
+    // for (let i=0;i<=20;i+=2){
+    //     console.log(`Even ${i}`);
+    // }
+
+    // for (let i=2000;i<=20000;i+=2000){
+    //     console.log(`Series ${i}`);
+    // }
+
+    // Question 8
+
+    // const bakery =["Bread","Samosa","Jalebi","mmm"]
+
+    // const userInput = prompt('Enter any item!');
+    // let isAvailable = false;
+
+    // for(let i=0; i < bakery.length; i++){
+    //     if(userInput === bakery[i]){
+    //         console.log('Item available hai!');
+    //         isAvailable = true;
+    //     }
+    // }
+
+    // if(!isAvailable){
+    //     console.log('item available nhin hai!');
+    // }
+
+
+    // Question 9
+
+    // function largestElement(arr) {
+    //     let largestNum = arr[0];
+    //     for (let i = 1; i < arr.length; i++) {
+    //         if (arr[i] > largestNum) {
+    //             largestNum = arr[i];
+    //         }
+    //     }
+    //     return largestNum;
+    // }
+    
+    // const num1 = [10, 15, 18, 20, 23];
+    // const result = largestElement(num1);
+    
+    // console.log("The largest element in the array is:" + result);
+
+
+    // Question 10
+
+    //  function smallestElement(arr) {
+    //     let smallestNum = arr[0];
+    //     for (let i = 1; i < arr.length; i++) {
+    //         if (arr[i] < smallestNum) {
+    //             smallestNum = arr[i];
+    //         }
+    //     }
+    //     return smallestNum;
+    // }
+    
+    // const num1 = [10, 15, 18, 20, 23];
+    // const result = smallestElement(num1);
+    
+    // console.log("The smallest element in the array is:" + result);
+
+    // Question 11
+
+    
+
+
+
+    
